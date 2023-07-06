97c02d8995600895eb85252207bfbd128ec8ac09
maxnodereal <max@nodereal.io>
2023-07-05 23:26:55 -0600

    Deploy website - based on 22187632bb55ccc80c4d0d95fde47f58887ea96a

---
+++ assets/js/814f3328.3a884281.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[535],{5641:e=>{e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"AvengerDAO June 26th Weekly Report","permalink":"/avgDAO-docs/blog/riskreport-20230619"}]}')}}]);
